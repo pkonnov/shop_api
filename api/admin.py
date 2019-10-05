9f107e9782ca1d232a382aae9127255e7a112890
Pavel Konnov <4itonada@gmail.com>
2019-10-05 18:45:23 +0400

    CategoryProduct prepopulated_fields

--- api/admin.py
+++ api/admin.py
@@ -2,8 +2,17 @@ from django.contrib import admin
 from .models import User, CategoryProduct, Product, Order, UserWallet
 
 
+@admin.register(CategoryProduct)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'category_name',
+        'category_url',
+    )
+    # search_fields = ('category_name', 'category_url')
+    prepopulated_fields = {'category_url': ['category_name']}
+
+
 admin.site.register(User)
-admin.site.register(CategoryProduct)
 admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(UserWallet)
